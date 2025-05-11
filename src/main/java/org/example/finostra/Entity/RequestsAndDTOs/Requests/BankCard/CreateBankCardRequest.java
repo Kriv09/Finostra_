96d1c5504393ca1f7cfeef5438e2808cc30b3b73
nazar-kryvychko <110460905+Kriv09@users.noreply.github.com>
2025-05-11 12:40:07 +0300

    Auth parameters and /me for login

--- src/main/java/org/example/finostra/Entity/RequestsAndDTOs/Requests/BankCard/CreateBankCardRequest.java
+++ src/main/java/org/example/finostra/Entity/RequestsAndDTOs/Requests/BankCard/CreateBankCardRequest.java
@@ -12,6 +12,5 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class CreateBankCardRequest {
-    private String publicUUID;
     private CurrencyType currency;
 }
--- src/main/java/org/example/finostra/Entity/RequestsAndDTOs/Responses/GetBankCardResponse.java
+++ src/main/java/org/example/finostra/Entity/RequestsAndDTOs/Responses/GetBankCardResponse.java
@@ -1,18 +1,21 @@
 package org.example.finostra.Entity.RequestsAndDTOs.Responses;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 
 @Builder
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetBankCardResponse {
     private List<CardInfo> cards;
 
     public record CardInfo(String cardNumber, String CVV, LocalDate expired) {}
+
+    public static GetBankCardResponse EMPTY = new GetBankCardResponse(Collections.emptyList());
 }
